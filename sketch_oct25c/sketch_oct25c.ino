9f6384de2055e2a47f554d31fbbe4332e7cb64a9
Soup-5 <93955560+Soup-5@users.noreply.github.com>
2023-11-03 12:53:34 -0700

    Update sketch_oct25c.ino

--- sketch_oct25c/sketch_oct25c.ino
+++ sketch_oct25c/sketch_oct25c.ino
@@ -1,20 +1,52 @@
+/*
+ * Useless Box stepper motor control script
+ * Cole Saldanha
+ * Last updated 11/3/2023
+ * Licensed under the MIT license
+ */
+
 #include <Stepper.h>
-#define STEP_SIZE 2048
+
+/*
+ * A 28BYJ-48 motor has a step size
+ * of 11.25 degrees, and the motor has a
+ * 1/64 reduction gear set which equates
+ * to a step size of 2048 per 1 full
+ * revolution. In other words, do
+ * not change this
+ */
+#define STEP_SIZE 2048 
 
 Stepper stepper(STEP_SIZE, 4, 6, 5, 7);
+
 const int switchPin = 10;
+const int ledLeft = 0;
+const int ledRight = 0;
+
+bool priorState = false;
 
 void setup() {
+  Serial.begin(9600);
   pinMode(switchPin, INPUT_PULLUP);
+
+  pinMode(ledLeft, OUTPUT);
+  pinMode(ledRight, OUTPUT);
+  
   stepper.setSpeed(10);
 }
 
 void loop() {
   bool switchState = !digitalRead(switchPin);
-  if (switchState == HIGH) {
-    stepper.step(-1);
+  if (switchState == HIGH && switchState != priorState) {
+    Serial.print("on\n");
+    stepper.step(STEP_SIZE);
+    priorState = switchState;
+    delay(500);
   }
-  else {
-    stepper.step(1);
+  else if (switchState == LOW && switchState != priorState) {
+    Serial.print("off\n");
+    stepper.step(-STEP_SIZE);
+    priorState = switchState;
+    delay(500);
   }
 }
